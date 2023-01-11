fc5cc2c8878ef8b531d5245dd4fb4f80d20b2798
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-11 23:36:52 +0300

    strict mode sample

--- src/components/HelloWorld/index.ts
+++ src/components/HelloWorld/index.ts
@@ -1,23 +1,30 @@
-import Component from '@glimmer/component/addon/-private/component';
-import { tracked }  from './../../config/ember';
-import { precompileTemplate } from '@ember/template-compilation';
-import Local from './local';
-import './style.css';
+import Component from "@glimmer/component/addon/-private/component";
+import { tracked } from "./../../config/ember";
+import { precompileTemplate } from "@ember/template-compilation";
+import Local from "./local";
+import "./style.css";
 export default class HelloWorld extends Component {
-    @tracked _date = new Date().toString();
-    Local = Local;
-    constructor() {
-        super(...arguments);
-        setInterval(() => {
-            this._date = new Date().toString();
-        }, 1000);
-    }
-    get date() {
-        return this._date;
-    }
-    static template = precompileTemplate(`
+  @tracked _date = new Date().toString();
+  constructor() {
+    super(...arguments);
+    setInterval(() => {
+      this._date = new Date().toString();
+    }, 1000);
+  }
+  get date() {
+    return this._date;
+  }
+  static template = precompileTemplate(
+    `
         <h1 class="hello-world">Hello World (from component)</h1>
         <pre>{{this.date}}</pre>
-        <this.Local />
-    `)
+        <Local />
+    `,
+    {
+      isStrictMode: true,
+      scope: () => ({
+        Local,
+      }),
+    }
+  );
 }
