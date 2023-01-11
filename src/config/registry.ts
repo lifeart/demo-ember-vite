1735e56f246ed14a2153284a9b0792581a53f8c6
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-11 21:48:17 +0300

    simplify component template registration

--- src/config/registry.ts
+++ src/config/registry.ts
@@ -1,16 +1,19 @@
 import { ApplicationController } from '../controllers/application';
 import { ApplicationRoute } from '../routes/application';
 import ApplicationTemplate from '../templates/application';
-
+import Ember from './ember';
 import HelloWorld from '../components/HelloWorld';
+
+function registerComponent(component) {
+    return Ember._setComponentTemplate(window.compile(component.template), component);
+}
 
 function registry() {
     return {
         'controller:application': ApplicationController,
         'route:application': ApplicationRoute,
         'template:application': ApplicationTemplate(),
-        'template:components/hello-world': window.compile(HelloWorld.template),
-        'component:hello-world': HelloWorld,
+        'component:hello-world': registerComponent(HelloWorld),
     };
 }
 
