b4970375ef516bb07e3968f4cfc6a49aed123496
Stanislav Romanov <kaermorchen@gmail.com>
2023-01-12 17:01:34 +0300

    Add initializers and instance initializers

--- src/config/initializer.ts
+++ src/config/initializer.ts
@@ -1,28 +1,35 @@
-import ENV from "./env";
-import registry from "./registry";
+import ENV from './env';
+import registry from './registry';
 import type Application from '@ember/application';
-import { registerComponent } from "./utils";
+import { registerComponent } from './utils';
+import { default as initializer } from '../initializer/logger';
+import { default as instanceInitializer } from '../instance-initializers/logger';
 
 export function init(application: Application, router: any) {
-    const MyApp = application.create({
-        "name": ENV.modulePrefix,
-        "version": "0.0.0+33d058ab"
-    });
-    
+  // Init initializers
+  application.initializer(initializer);
 
-    const registryObjects = registry();
-    console.table(registryObjects);
-    
-    Object.keys(registryObjects).forEach((key) => {
-        const value = registryObjects[key];
-        if (key.startsWith('component:')) {
-            MyApp.register(key, registerComponent(value));
-        } else {
-            MyApp.register(key, value);
-        }
-    });
+  // Init instance initializers
+  application.instanceInitializer(instanceInitializer);
 
-    MyApp.register('router:main', router);
+  const MyApp = application.create({
+    name: ENV.modulePrefix,
+    version: '0.0.0+33d058ab',
+  });
 
-    return MyApp;
+  const registryObjects = registry();
+  console.table(registryObjects);
+
+  Object.keys(registryObjects).forEach((key) => {
+    const value = registryObjects[key];
+    if (key.startsWith('component:')) {
+      MyApp.register(key, registerComponent(value));
+    } else {
+      MyApp.register(key, value);
+    }
+  });
+
+  MyApp.register('router:main', router);
+
+  return MyApp;
 }
