0a7e4309087a1855be2dec4c4d9f67d2ab785135
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-12 15:33:21 +0300

    support environment modes in app config

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,16 +1,22 @@
-const ENV = {
-  modulePrefix: "vite-ember",
-  environment: "development",
-  rootURL: "/",
-  locationType: "history", // here is the change
-  EmberENV: {
-    FEATURES: {},
-    EXTEND_PROTOTYPES: {
-      Date: false,
+function config(environment: 'production' | 'development') {
+
+  const ENV = {
+    modulePrefix: "vite-ember",
+    environment,
+    rootURL: "/",
+    locationType: "history", // here is the change
+    EmberENV: {
+      FEATURES: {},
+      EXTEND_PROTOTYPES: {
+        Date: false,
+      },
     },
-  },
+    APP: {},
+  };
 
-  APP: {},
-};
+  return ENV;
+}
 
+
+const ENV = config(import.meta.env.MODE as 'production' | 'development');
 export default ENV;
