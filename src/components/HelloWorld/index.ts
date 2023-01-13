23bea82c9e66c9f08fb3c6717aa1f09ab351ee5b
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-13 18:04:24 +0300

    change locale buttons

--- src/components/HelloWorld/index.ts
+++ src/components/HelloWorld/index.ts
@@ -6,10 +6,15 @@ import type DateService from '../../services/date';
 import TemplateOnlyComponent from '@/components/OnlyTemplate/component.hbs';
 import './style.css';
 import Select from '@/components/Select';
-
+import type IntlService from 'ember-intl/addon/services/intl';
 const calculateSummary = (a: number, b: number) => `${a}+${b}=${a + b}`;
 export default class HelloWorld extends Component {
   @service('date') dateService!: DateService;
+  @service('intl') intl!: IntlService;
+
+  setLanguage = (language: string) => {
+    this.intl.setLocale([language]);
+  }
 
   static template = precompileTemplate(
     `
@@ -25,6 +30,10 @@ export default class HelloWorld extends Component {
       <br>
       {{calculateSummary 3 2}}
       <br>
+
+      <Button {{on "click" (fn this.setLanguage "en-us")}}>en-us</Button>
+      <Button {{on "click" (fn this.setLanguage "fr-fr")}}>fr-fr</Button>
+      <Button {{on "click" (fn this.setLanguage "ru-ru")}}>ru-ru</Button>
 
       <Select />
     `,
