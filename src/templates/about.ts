8c35c1375991e58c1053ac79547eaf18e67e898f
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-13 09:10:48 +0300

    more hbs samples

--- src/templates/about.ts
+++
@@ -1,9 +0,0 @@
-import { precompileTemplate } from '@ember/template-compilation';
-
-
-export default precompileTemplate(`
-    <h2>About</h2>
-    <p>Ember Application, powered by Vite</p>
-    <LinkTo @route="main">Home</LinkTo>
-
-`);
