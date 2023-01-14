f4d38fb89d63dc302aef109f0add552c17d04903
Stanislav Romanov <kaermorchen@gmail.com>
2023-01-14 13:51:47 +0300

    Init auth

--- src/addons/ember-simple-auth.ts
+++ src/addons/ember-simple-auth.ts
@@ -1,8 +1,18 @@
-
-import SessionService from "ember-simple-auth/addon/services/session";
+import SessionService from 'ember-simple-auth/addon/services/session';
+import SessionStoresApplication from 'ember-simple-auth/app/session-stores/application';
+import Adaptive from 'ember-simple-auth/session-stores/adaptive';
+import LocalStorage from 'ember-simple-auth/session-stores/local-storage';
+import Cookie from 'ember-simple-auth/session-stores/cookie';
+import InternalSession from 'ember-simple-auth/internal-session';
 
 const registry = {
-    'service:session': SessionService,
-}
+  'session-store:adaptive': Adaptive,
+  'session-store:cookie': Cookie,
+  'session-store:local-storage': LocalStorage,
+  'session:main': InternalSession,
+  'session-store:application': SessionStoresApplication,
+  'service:session': SessionService,
+  'service:cookies': Cookie,
+};
 
 export default registry;
--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig(({ mode }) => {
             extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.hbs'],
             alias: [
                 { find: 'ember-simple-auth/use-session-setup-method', replacement: './compat/ember-simple-auth/use-session-setup-method.ts' },
+                { find: /ember-simple-auth\/(?!(app|addon)\/)(.+)/, replacement: 'ember-simple-auth/addon/$2' },
                 // { find: 'ember-intl/translations', replacement: fileURLToPath(new URL("./compat/empty-array", import.meta.url)) },
                 { find: 'ember-modifier', replacement: fileURLToPath(new URL("./node_modules/ember-modifier/dist", import.meta.url)) },
                 // { find: '@glimmer/manager', replacement: '@glimmer/manager/dist/modules/es2017' },
