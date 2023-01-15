6cdbf3859195b84139dfea0eaaf0b1a4bc492df2
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-15 15:03:16 +0300

    more improvements

---
+++ compat/ember-data-private-build-infra/debugging.ts
@@ -0,0 +1,7 @@
+export const LOG_PAYLOADS = false;
+export const LOG_IDENTIFIERS = true;
+export const LOG_NOTIFICATIONS = false;
+export const LOG_INSTANCE_CACHE = false;
+export const LOG_GRAPH = true;
+export const LOG_MUTATIONS = false;
+export const LOG_OPERATIONS = false;
---
+++ compat/ember-data-private-build-infra/index.ts
@@ -0,0 +1,3 @@
+export const HAS_DEBUG_PACKAGE = true;
+export const HAS_MODEL_PACKAGE = true;
+export const HAS_RECORD_DATA_PACKAGE = true;
