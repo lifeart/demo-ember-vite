41d3a53f45e6c34b9168c838751621ad6cfd673a
Alex Kanunnikov <lifeart92@gmail.com>
2023-01-18 15:50:07 +0300

    fix ember-template-lint errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,17 @@
 
 export default {
   extends: 'recommended',
+  rules: {
+    'no-curly-component-invocation': { allow: ['memory-usage'] },
+    'no-implicit-this': { allow: ['memory-usage'] },
+  },
+  overrides: [
+    {
+      files: ['./src/**/*.gts'],
+      rules: {
+        'no-curly-component-invocation': 'off',
+        'no-implicit-this': 'off',
+      },
+    },
+  ],
 };
