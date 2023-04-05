0f8936ce77b27d49ad7ee8ce54e19b4d08f43472
Alex Kanunnikov <lifeart92@gmail.com>
2023-04-05 15:35:42 +0300

    test for not-found route

---
+++ e2e/flows/404.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+import { captureCoverage } from './../utils/index.ts';
+
+captureCoverage(test);
+
+test.describe('NotFound route', () => {
+  test('it loadable', async ({ page }) => {
+    await page.goto('http://localhost:4200/foo-bar-baz');
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Not found/);
+    // Expect an element "to contain" text.
+    await expect(page.locator('[data-not-found-text]')).toContainText(
+      'Sorry, we couldn’t find the page /foo-bar-baz'
+    );
+  });
+});
--- src/templates/not-found.ts
+++ src/templates/not-found.ts
@@ -4,6 +4,7 @@ const currentPath = () => window.location.pathname;
 
 export default precompileTemplate(
   `
+  {{page-title "Not found"}}
   <div class="flex min-h-full flex-col bg-white pt-16 pb-12">
   <main class="mx-auto flex w-full max-w-7xl flex-grow flex-col justify-center px-6 lg:px-8">
     <div class="flex flex-shrink-0 justify-center">
@@ -16,7 +17,7 @@ export default precompileTemplate(
       <div class="text-center">
         <p class="text-base font-semibold text-indigo-600">404</p>
         <h1 class="mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found.</h1>
-        <p class="mt-2 text-base text-gray-500">Sorry, we couldn’t find the page {{currentPath}}</p>
+        <p class="mt-2 text-base text-gray-500" data-not-found-text>Sorry, we couldn’t find the page {{currentPath}}</p>
         <div class="mt-6">
           <LinkTo @route="main" class="text-base font-medium text-indigo-600 hover:text-indigo-500">
           Go back home
