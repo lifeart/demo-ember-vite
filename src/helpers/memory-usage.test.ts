9afaa6401b97cf2e9d39470cd3a7f2efe34c996f
Alex Kanunnikov <lifeart92@gmail.com>
2023-04-05 10:25:32 +0300

    jest sample

---
+++ src/helpers/memory-usage.test.ts
@@ -0,0 +1,24 @@
+// jest test for memory-usage.ts
+import MemoryUsage from './memory-usage';
+
+describe('helpers | memory-usage', () => {
+  it('should retun from computation if no performance property', () => {
+    const memoryUsage = new MemoryUsage();
+    expect(memoryUsage.compute()).toBeNull();
+  });
+  it('will destroy should clear interval', async () => {
+    const memoryUsage = new MemoryUsage();
+    let a = 1;
+    const interval = setInterval(() => {
+      a++;
+      // NOOP
+    }, 10);
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    memoryUsage.interval = interval;
+    expect(memoryUsage.interval).toBe(interval);
+    const lastIntervalValue = a;
+    memoryUsage.willDestroy();
+    await new Promise((resolve) => setTimeout(resolve, 100));
+    expect(a).toBe(lastIntervalValue);
+  });
+});
