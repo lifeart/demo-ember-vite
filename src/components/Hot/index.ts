45b14f185e8f031409a0190497fa004af7f1e5ee
Alex Kanunnikov <lifeart92@gmail.com>
2023-07-14 10:33:23 +0300

    get stable render

--- src/components/Hot/index.ts
+++ src/components/Hot/index.ts
@@ -1,23 +1,40 @@
 import Component from '@glimmer/component';
 import { precompileTemplate } from '@ember/template-compilation';
 import { tracked } from '@glimmer/tracking';
-export default class Hot extends Component {
+import { registerDestructor } from '@ember/destroyable';
+
+// eslint-disable-next-line no-var
+var GlobalRefCache: Record<string, unknown> = {};
+interface Args {
+  component: unknown;
+  module: string;
+}
+export default class Hot extends Component<Args> {
   @tracked isVisible = true;
   revision = 0;
-  @tracked originalComponent = this.args.component;
+  @tracked originalComponent: null | unknown = null;
   get component() {
     return this.originalComponent;
   }
   constructor() {
     super(...arguments);
-    window.addEventListener('hot-reload', (a) => {
-      const detail = a.detail;
-      const ref  = this.args.module;
+
+    const { component, module } = this.args;
+    this.originalComponent = GlobalRefCache[module] || component;
+
+    const fn = (a: Event) => {
+      const detail = (a as unknown as CustomEvent).detail;
+      const ref = module;
       const target = '/' + detail.moduleName.split('/src/')[1];
       if (ref === target) {
         this.revision++;
+        GlobalRefCache[module] = detail.component;
         this.originalComponent = detail.component;
       }
+    };
+    window.addEventListener('hot-reload', fn);
+    registerDestructor(this, () => {
+      window.removeEventListener('hot-reload', fn);
     });
   }
   static template = precompileTemplate(`
