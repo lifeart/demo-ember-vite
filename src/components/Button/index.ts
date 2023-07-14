48844d750d82dbd597d497029e7a59551b6285c7
Alex Kanunnikov <lifeart92@gmail.com>
2023-07-14 11:21:24 +0300

    simplify refs

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -4,12 +4,12 @@ import { getComponentTemplate } from '@glimmer/manager';
 
 export default class Button extends Component {
   static template = template;
-  a = 's';
+  a = '';
 }
 
 if (import.meta.hot) {
   import.meta.hot.accept((newModule) => {
-    const tpl = getComponentTemplate(Button.template);
+    const tpl = getComponentTemplate(newModule.default);
     const moduleName = tpl().moduleName;
     window.dispatchEvent(
       new CustomEvent('hot-reload', {
