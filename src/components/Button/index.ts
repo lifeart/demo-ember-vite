648dd096c2e6593f5e23b89d35598682dbcdd6a4
Alex Kanunnikov <lifeart92@gmail.com>
2023-07-14 03:36:12 +0300

    more realistic case

--- src/components/Button/index.ts
+++ src/components/Button/index.ts
@@ -1,44 +1,38 @@
 import Component from '@glimmer/component';
 import template from './template.hbs';
-import env from '@/config/env';
 import { getComponentTemplate } from '@glimmer/manager';
 
 export default class Button extends Component {
   static template = template;
+  a = '';
 }
 
 if (import.meta.hot) {
-  import.meta.hot.accept('./template.hbs', (newFoo) => {
-    const app = Array.from(
-      window[env.APP.globalName]._applicationInstances.values()
-    )[0];
-    const renderer = app.lookup('renderer:-dom');
-    console.log(app, renderer);
-    renderer._runtimeResolver.componentDefinitionCache.clear();
-    renderer._context.constants.componentDefinitionCount = 0;
-    renderer._context.constants.componentDefinitionCache = new WeakMap();
-
-    renderer._runtime.program.constants.componentDefinitionCache =
-      new WeakMap();
-
-    Button.template = getComponentTemplate(newFoo.default);
-    const tpl = Button.template(app).asLayout();
-    const moduleName = tpl.moduleName;
-    renderer._context.constants.values = renderer._context.constants.values.map(
-      (e) => {
-        if (e?.moduleName && e.moduleName === tpl.moduleName) {
-          renderer._context.constants.indexMap.set(
-            tpl,
-            renderer._context.constants.indexMap.get(e)
-          );
-          renderer._context.constants.indexMap.delete(e);
-          return tpl;
-        } else {
-          return e;
-        }
-      }
+  import.meta.hot.accept((newModule) => {
+    const tpl = getComponentTemplate(Button.template);
+    const moduleName = tpl(undefined).moduleName;
+    window.dispatchEvent(
+      new CustomEvent('hot-reload', {
+        detail: {
+          moduleName,
+          component: newModule.default,
+        },
+      })
     );
-
-    window.dispatchEvent(new CustomEvent('hot-reload', { detail: moduleName }));
+  });
+  import.meta.hot.accept('./template.hbs', (newFoo) => {
+    const tpl = getComponentTemplate(newFoo.default);
+    const moduleName = tpl(undefined).moduleName;
+    class NewComponent extends Button {
+      static template = tpl;
+    }
+    window.dispatchEvent(
+      new CustomEvent('hot-reload', {
+        detail: {
+          moduleName,
+          component: NewComponent,
+        },
+      })
+    );
   });
 }
--- src/components/Hot/index.ts
+++ src/components/Hot/index.ts
@@ -3,22 +3,24 @@ import { precompileTemplate } from '@ember/template-compilation';
 import { tracked } from '@glimmer/tracking';
 export default class Hot extends Component {
   @tracked isVisible = true;
+  revision = 0;
+  @tracked originalComponent = this.args.component;
+  get component() {
+    return this.originalComponent;
+  }
   constructor() {
     super(...arguments);
     window.addEventListener('hot-reload', (a) => {
       const detail = a.detail;
       const ref  = this.args.module;
-      const target = '/' + detail.split('/src/')[1];
+      const target = '/' + detail.moduleName.split('/src/')[1];
       if (ref === target) {
-        this.isVisible = false;
-        setTimeout(() => {
-            this.isVisible = true;
-        }, 10);
-        console.log('hot reload', ref, target);
+        this.revision++;
+        this.originalComponent = detail.component;
       }
     });
   }
   static template = precompileTemplate(`
-        {{#if this.isVisible}}{{yield}}{{/if}}
+    {{yield this.component}}
   `);
 }
